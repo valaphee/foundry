1d966ed8a50a0c405da0a59f3c362b3bd6c9ffd3
Kevin Ludwig <arKiVdf7YjUI6h4M5k3yTMmgDahGbK9G@valaphee.com>
2021-06-28 19:18:34 +0200

    optimization

--- foundry-math/src/main/kotlin/com/valaphee/foundry/math/noise/SimplexNoise.kt
+++ foundry-math/src/main/kotlin/com/valaphee/foundry/math/noise/SimplexNoise.kt
@@ -12,10 +12,10 @@ import com.valaphee.foundry.math.fastFloor
  */
 class SimplexNoise : Noise {
     override fun get(seed: Int, x: Float, y: Float): Float {
-        var t = (x + y) * F2F
+        var t = (x + y) * f2f
         val i = fastFloor(x + t)
         val j = fastFloor(y + t)
-        t = (i + j) * G2F
+        t = (i + j) * g2f
         val X0 = i - t
         val Y0 = j - t
         val x0 = x - X0
@@ -29,10 +29,10 @@ class SimplexNoise : Noise {
             i1 = 0
             j1 = 1
         }
-        val x1 = x0 - i1 + G2F
-        val y1 = y0 - j1 + G2F
-        val x2 = x0 - 1.0f + F2F
-        val y2 = y0 - 1.0f + F2F
+        val x1 = x0 - i1 + g2f
+        val y1 = y0 - j1 + g2f
+        val x2 = x0 - 1.0f + f2f
+        val y2 = y0 - 1.0f + f2f
         val n0: Float
         val n1: Float
         val n2: Float
@@ -41,12 +41,12 @@ class SimplexNoise : Noise {
             t *= t
             n0 = t * t * gradHash(seed, i, j, x0, y0)
         }
-        t = 0.5.toFloat() - x1 * x1 - y1 * y1
+        t = 0.5f - x1 * x1 - y1 * y1
         if (t < 0.0f) n1 = 0.0f else {
             t *= t
             n1 = t * t * gradHash(seed, i + i1, j + j1, x1, y1)
         }
-        t = 0.5.toFloat() - x2 * x2 - y2 * y2
+        t = 0.5f - x2 * x2 - y2 * y2
         if (t < 0.0f) n2 = 0.0f else {
             t *= t
             n2 = t * t * gradHash(seed, i + 1, j + 1, x2, y2)
@@ -55,11 +55,11 @@ class SimplexNoise : Noise {
     }
 
     override fun get(seed: Int, x: Float, y: Float, z: Float): Float {
-        var t = (x + y + z) * F3F
+        var t = (x + y + z) * f3f
         val i = fastFloor(x + t)
         val j = fastFloor(y + t)
         val k = fastFloor(z + t)
-        t = (i + j + k) * G3F
+        t = (i + j + k) * g3f
         val x0 = x - (i - t)
         val y0 = y - (j - t)
         val z0 = z - (k - t)
@@ -120,15 +120,15 @@ class SimplexNoise : Noise {
                 k2 = 0
             }
         }
-        val x1 = x0 - i1 + G3F
-        val y1 = y0 - j1 + G3F
-        val z1 = z0 - k1 + G3F
-        val x2 = x0 - i2 + F3F
-        val y2 = y0 - j2 + F3F
-        val z2 = z0 - k2 + F3F
-        val x3 = x0 + G33F
-        val y3 = y0 + G33F
-        val z3 = z0 + G33F
+        val x1 = x0 - i1 + g3f
+        val y1 = y0 - j1 + g3f
+        val z1 = z0 - k1 + g3f
+        val x2 = x0 - i2 + f3f
+        val y2 = y0 - j2 + f3f
+        val z2 = z0 - k2 + f3f
+        val x3 = x0 + g33f
+        val y3 = y0 + g33f
+        val z3 = z0 + g33f
         val n0: Float
         val n1: Float
         val n2: Float
@@ -157,14 +157,10 @@ class SimplexNoise : Noise {
     }
 
     companion object {
-        private const val F2D = 1.0 / 2.0
-        private const val F2F = F2D.toFloat()
-        private const val G2D = 1.0 / 4.0
-        private const val G2F = G2D.toFloat()
-        private const val F3D = 1.0 / 3.0
-        private const val F3F = F3D.toFloat()
-        private const val G3D = 1.0 / 6.0
-        private const val G3F = G3D.toFloat()
-        private const val G33F = G3F * 3 - 1
+        private const val f2f = 1.0f / 2.0f
+        private const val g2f = 1.0f / 4.0f
+        private const val f3f = 1.0f / 3.0f
+        private const val g3f = 1.0f / 6.0f
+        private const val g33f = g3f * 3.0f - 1.0f
     }
 }
