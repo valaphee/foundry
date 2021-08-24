ba8d731ffd107f888d54729d019a1d5143596941
Kevin Ludwig <signing@valaphee.com>
2021-08-24 19:52:21 +0200

    License update to MIT License

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,25 @@
 /*
+ * MIT License
+ *
  * Copyright (c) 2021, Valaphee.
- * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -9,6 +28,7 @@ plugins {
     id("com.palantir.git-version") version "0.12.3"
     `java-library`
     kotlin("jvm") version "1.5.30-M1"
+    id("net.linguica.maven-settings") version "0.5"
     `maven-publish`
     signing
 }
@@ -60,38 +80,5 @@ subprojects {
     signing {
         useGpgCmd()
         sign(publishing.publications)
-    }
-
-    publishing {
-        publications {
-            create<MavenPublication>("maven") {
-                pom.apply {
-                    name.set(project.name)
-                    description.set("")
-                    url.set("https://github.com/valaphee/foundry")
-                    scm {
-                        connection.set("https://github.com/valaphee/foundry.git")
-                        developerConnection.set("https://github.com/valaphee/foundry.git")
-                        url.set("https://github.com/valaphee/foundry")
-                    }
-                    licenses {
-                        license {
-                            name.set("All rights reserved")
-                            url.set("https://raw.githubusercontent.com/valaphee/foundry/master/LICENSE.txt")
-                        }
-                    }
-                    developers {
-                        developer {
-                            id.set("valaphee")
-                            name.set("Valaphee")
-                            email.set("iam@valaphee.com")
-                            roles.add("owner")
-                        }
-                    }
-                }
-
-                from(components["java"])
-            }
-        }
     }
 }
--- foundry-math/build.gradle.kts
+++ foundry-math/build.gradle.kts
@@ -1,6 +1,58 @@
 /*
+ * MIT License
+ *
  * Copyright (c) 2021, Valaphee.
- * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 dependencies { implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable-jvm:0.3.4") }
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            pom.apply {
+                name.set("Foundry Math")
+                description.set("A library of lightweight, self-contained mathematics addressing the most common problems not available in the Java/Kotlin programming language.")
+                url.set("https://valaphee.com")
+                scm {
+                    connection.set("https://github.com/valaphee/foundry.git")
+                    developerConnection.set("https://github.com/valaphee/foundry.git")
+                    url.set("https://github.com/valaphee/foundry")
+                }
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://raw.githubusercontent.com/valaphee/foundry/master/LICENSE.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("valaphee")
+                        name.set("Valaphee")
+                        email.set("iam@valaphee.com")
+                        roles.add("owner")
+                    }
+                }
+            }
+
+            from(components["java"])
+        }
+    }
+}
