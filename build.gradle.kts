3e5d3209698c3b845d00396dfd0406ca68c3319f
Kevin Ludwig <signing@valaphee.com>
2022-01-16 11:29:25 +0100

    Update copyright, re-license to Apache

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,48 +1,37 @@
 /*
- * MIT License
+ * Copyright (c) 2021-2022, Valaphee.
  *
- * Copyright (c) 2021, Valaphee.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("com.palantir.git-version") version "0.12.3"
-    `java-library`
     kotlin("jvm") version "1.5.30-M1"
-    id("net.linguica.maven-settings") version "0.5"
     `maven-publish`
     signing
 }
 
 allprojects {
     repositories {
-        mavenLocal()
         mavenCentral()
+        mavenLocal()
     }
 }
 
 subprojects {
     apply(plugin = "com.palantir.git-version")
-    apply(plugin = "java-library")
     apply(plugin = "kotlin")
     apply(plugin = "maven-publish")
     apply(plugin = "signing")
@@ -58,12 +47,7 @@ subprojects {
             targetCompatibility = "16"
         }
 
-        withType<KotlinCompile> {
-            kotlinOptions {
-                jvmTarget = "16"
-                freeCompilerArgs = listOf("-Xlambdas=indy", "-Xsam-conversions=indy")
-            }
-        }
+        withType<KotlinCompile> { kotlinOptions { jvmTarget = "16" } }
 
         withType<Test> { useJUnitPlatform() }
     }
